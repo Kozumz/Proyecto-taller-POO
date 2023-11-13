4e413a69f89cba0898e20d958127e6a5619ca110
Kozumz <cjmm14mm@gmail.com>
2023-11-13 02:24:26 -0600

    Punto de venta casi finalizado

--- src/FuncionesSQL.java
+++ src/FuncionesSQL.java
@@ -1,17 +1,40 @@
 package src;
 import java.sql.*;
+import javax.swing.*;
+
 
 public class FuncionesSQL {
 
-    static void GuardarEnBase(String dbNAme, String dbTable,String values,Pestaña x){
+    static void GuardarEnBase(String dbNAme, String dbTable,Pestaña x){
         try{
             Connection NuevaConexion = DriverManager.getConnection("jdbc:mysql://localhost/"+ dbNAme,"root","");
-            PreparedStatement NuevoEstado = NuevaConexion.prepareStatement("insert into "+dbTable + values);
-
+            PreparedStatement NuevoEstado = NuevaConexion.prepareStatement("insert into " +dbTable + " values (?,?,?,?,?)" );
+            //
             NuevoEstado.setString(1,"0");
             NuevoEstado.setString(2,"");
         }catch (Exception e){
 
         }
     }
+
+    static void LeerDeBase(String dbNAme, String dbTable,Producto x,String id){
+        try{
+            Connection NuevaConexion = DriverManager.getConnection("jdbc:mysql://localhost/"+ dbNAme,"root","");
+            PreparedStatement NuevoEstado = NuevaConexion.prepareStatement("select * from "+dbTable + " where id = ? " );
+
+            NuevoEstado.setString(1, id);
+
+            ResultSet rs = NuevoEstado.executeQuery();
+
+            if (rs.next()){
+                x.NombreDeProducto = (rs.getString("NombreProducto"));
+                x.Descripcion = (rs.getString("Descripcion"));
+                x.Existencias = (rs.getInt("Existencias"));
+                x.Precio = (rs.getInt("Precio"));
+            }
+        }catch (Exception e){
+
+        }
+    }
+
 }
